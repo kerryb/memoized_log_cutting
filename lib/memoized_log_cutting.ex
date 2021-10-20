da113c576e8ecdbb395a0ca5d23c12fc4dba9479
Kerry Buckley <kerryjbuckley@gmail.com>
2021-10-20 22:47:37 +0100

    Implement naive (unmemoized) solution

--- lib/memoized_log_cutting.ex
+++ lib/memoized_log_cutting.ex
@@ -1,2 +1,9 @@
 defmodule MemoizedLogCutting do
+  def cut_log(_prices, 0), do: 0
+
+  def cut_log(prices, length) do
+    Enum.reduce(1..length, 0, fn cut_length, max_price ->
+      max(max_price, prices[cut_length] + cut_log(prices, length - cut_length))
+    end)
+  end
 end
--- test/memoized_log_cutting_test.exs
+++ test/memoized_log_cutting_test.exs
@@ -1,3 +1,19 @@
 defmodule MemoizedLogCuttingTest do
   use ExUnit.Case
+
+  describe "MemoizedLogCutting.cut_log/1" do
+    @p %{0 => 0, 1 => 1, 2 => 5, 3 => 8, 4 => 9, 5 => 10}
+
+    test "returns the expected value for simple case" do
+      assert MemoizedLogCutting.cut_log(@p, 1) == 1
+    end
+
+    test "returns the highest possible price when it corresponds to a single length" do
+      assert MemoizedLogCutting.cut_log(@p, 2) == 5
+    end
+
+    test "returns the highest possible price when it requires multiple lengths" do
+      assert MemoizedLogCutting.cut_log(@p, 5) == 13
+    end
+  end
 end
