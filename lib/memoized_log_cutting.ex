d546469d0f80eb31d359e000e381999136ef3c7f
Kerry Buckley <kerryjbuckley@gmail.com>
2021-10-20 20:21:36 +0100

    Remove generated boilerplate

--- lib/memoized_log_cutting.ex
+++ lib/memoized_log_cutting.ex
@@ -1,18 +1,2 @@
 defmodule MemoizedLogCutting do
-  @moduledoc """
-  Documentation for `MemoizedLogCutting`.
-  """
-
-  @doc """
-  Hello world.
-
-  ## Examples
-
-      iex> MemoizedLogCutting.hello()
-      :world
-
-  """
-  def hello do
-    :world
-  end
 end
--- test/memoized_log_cutting_test.exs
+++ test/memoized_log_cutting_test.exs
@@ -1,8 +1,3 @@
 defmodule MemoizedLogCuttingTest do
   use ExUnit.Case
-  doctest MemoizedLogCutting
-
-  test "greets the world" do
-    assert MemoizedLogCutting.hello() == :world
-  end
 end
