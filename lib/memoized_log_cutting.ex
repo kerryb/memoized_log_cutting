ab2f2104a2a9649b96b46fb5e5906f16fa5f99c3
Kerry Buckley <kerryjbuckley@gmail.com>
2021-10-23 18:53:28 +0100

    First (failed) attempt at memoisation
    Still works, but also still re-traversing all possibilities.

--- lib/memoized_log_cutting.ex
+++ lib/memoized_log_cutting.ex
@@ -1,9 +1,19 @@
 defmodule MemoizedLogCutting do
-  def cut_log(_prices, 0), do: 0
+  def cut_log(prices, length), do: do_cut_log(prices, length, %{})
 
-  def cut_log(prices, length) do
-    Enum.reduce(1..length, 0, fn cut_length, max_price ->
-      max(max_price, Map.get(prices, cut_length, 0) + cut_log(prices, length - cut_length))
+  defp do_cut_log(_prices, 0, _memo), do: 0
+
+  defp do_cut_log(prices, length, memo) do
+    Map.get_lazy(memo, length, fn ->
+      {price, _memo} =
+        Enum.reduce(1..length, {0, memo}, fn cut_length, {max_price, memo} ->
+          cut_length_price = Map.get(prices, cut_length, 0)
+          max_remainder_price = do_cut_log(prices, length - cut_length, memo)
+          price = max(max_price, cut_length_price + max_remainder_price)
+          {price, Map.put(memo, length, price)}
+        end)
+
+      price
     end)
   end
 end
