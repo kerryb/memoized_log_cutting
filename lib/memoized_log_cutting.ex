65c839d0d2b93ac98907fd256fcfbe24cdfe2ce8
Kerry Buckley <kerryjbuckley@gmail.com>
2021-10-27 22:07:56 +0100

    Extract variable

--- lib/memoized_log_cutting.ex
+++ lib/memoized_log_cutting.ex
@@ -13,8 +13,9 @@ defmodule MemoizedLogCutting do
 
   defp calculate_max_value(prices, length, memo) do
     Enum.reduce(1..length, {0, memo}, fn cut_length, {max_value, memo} ->
+      cut_value = Map.get(prices, cut_length, 0)
       {remainder_value, memo} = do_cut_log(prices, length - cut_length, memo)
-      value = max(max_value, Map.get(prices, cut_length, 0) + remainder_value)
+      value = max(max_value, cut_value + remainder_value)
       {value, Map.put(memo, length, value)}
     end)
   end
