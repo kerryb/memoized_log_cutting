a80e908b06778b5ce8171c6f953acad824f39699
Kerry Buckley <kerryjbuckley@gmail.com>
2021-10-27 20:41:37 +0100

    Extract calculation to a function

--- lib/memoized_log_cutting.ex
+++ lib/memoized_log_cutting.ex
@@ -6,15 +6,16 @@ defmodule MemoizedLogCutting do
 
   def do_cut_log(prices, length, memo) do
     case memo[length] do
-      nil ->
-        Enum.reduce(1..length, {0, memo}, fn cut_length, {max_value, memo} ->
-          {remainder_value, memo} = do_cut_log(prices, length - cut_length, memo)
-          value = max(max_value, Map.get(prices, cut_length, 0) + remainder_value)
-          {value, Map.put(memo, length, value)}
-        end)
-
-      value ->
-        {value, memo}
+      nil -> calculate_max_value(prices, length, memo)
+      value -> {value, memo}
     end
+  end
+
+  defp calculate_max_value(prices, length, memo) do
+    Enum.reduce(1..length, {0, memo}, fn cut_length, {max_value, memo} ->
+      {remainder_value, memo} = do_cut_log(prices, length - cut_length, memo)
+      value = max(max_value, Map.get(prices, cut_length, 0) + remainder_value)
+      {value, Map.put(memo, length, value)}
+    end)
   end
 end
